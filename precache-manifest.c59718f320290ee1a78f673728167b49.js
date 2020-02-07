4ff6f40e4a0bd223a783e1432d652020c8e4c100
Andrii <farikpite@gmail.com>
2020-02-07 03:56:12 +0200

    Updates

---
+++ precache-manifest.c59718f320290ee1a78f673728167b49.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f4ed6e3f2f51d8467b49af1147d6fc",
+    "url": "/Star_Wars/index.html"
+  },
+  {
+    "revision": "776efc6ef3e769c6a29f",
+    "url": "/Star_Wars/static/css/main.e29e23ed.chunk.css"
+  },
+  {
+    "revision": "77f47826f0f345de6027",
+    "url": "/Star_Wars/static/js/2.786cb14e.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/Star_Wars/static/js/2.786cb14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776efc6ef3e769c6a29f",
+    "url": "/Star_Wars/static/js/main.17b64042.chunk.js"
+  },
+  {
+    "revision": "105fd6f05a338b334466",
+    "url": "/Star_Wars/static/js/runtime-main.c9fe2f1f.js"
+  },
+  {
+    "revision": "017b5111bec0b270abf7761c4168e1ab",
+    "url": "/Star_Wars/static/media/Dart_Moul.017b5111.jpg"
+  },
+  {
+    "revision": "cb908b66dd417af4ae11d6a4ed8c9643",
+    "url": "/Star_Wars/static/media/Luke.cb908b66.jpg"
+  },
+  {
+    "revision": "022c4b282cfd8313c433820ed2a98504",
+    "url": "/Star_Wars/static/media/Mando.022c4b28.jpg"
+  },
+  {
+    "revision": "dd9544d3e0fbb4874dbb00aa4c52b595",
+    "url": "/Star_Wars/static/media/Ray.dd9544d3.jpg"
+  },
+  {
+    "revision": "a50e2585e6bcfa8bc5b624eabe97c363",
+    "url": "/Star_Wars/static/media/millenium-falcon-star-wars-model_Z.a50e2585.jpg"
+  },
+  {
+    "revision": "c5593d73eb319c748c45dec37d5691d8",
+    "url": "/Star_Wars/static/media/starShip.c5593d73.jpg"
+  },
+  {
+    "revision": "c19759a5042ae33a1ceb3a87efed741c",
+    "url": "/Star_Wars/static/media/vader.c19759a5.jpg"
+  },
+  {
+    "revision": "231cb6adfc7321d4bbd4688acdbeabad",
+    "url": "/Star_Wars/static/media/yoda.231cb6ad.jpg"
+  }
+]);
